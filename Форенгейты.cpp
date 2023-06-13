41181e98988ea92f4996c3102d9b6d982f62883a
Kalanho <136378818+Kalanho@users.noreply.github.com>
2023-06-13 18:38:45 +0300

    Add files via upload

---
+++ Форенгейты.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int per(double b);
+int main()
+{
+    double C;
+    cout << "C=";
+     cin >> C;
+    cout <<"F="<<per(C);
+    return 0;
+}
+int per(double b)
+{ 
+  return  b * 9 / 5 + 32;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
